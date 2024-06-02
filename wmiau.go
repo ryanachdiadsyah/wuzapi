89e7f8df4816a2a313882b8093bbe931e33aff8a
Ryan Achdiadsyah <achdiadsyah@gmail.com>
2024-06-02 22:23:22 +0700

    update os Name

--- wmiau.go
+++ wmiau.go
@@ -161,7 +161,7 @@ func (s *server) startClient(userID int, textjid string, token string, subscript
 	//store.CompanionProps.PlatformType = waProto.CompanionProps_CHROME.Enum()
 	//store.CompanionProps.Os = proto.String("Mac OS")
 
-	osName := "Wuzapi"
+	osName := "CDNCRM"
 	store.DeviceProps.PlatformType = waProto.DeviceProps_CHROME.Enum()
 	store.DeviceProps.Os = &osName
 
