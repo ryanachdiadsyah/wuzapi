b217ba2d49db515476efa6ba10a10f7d341c0dd4
Ryan Achdiadsyah <achdiadsyah@gmail.com>
2024-11-14 19:24:53 +0700

    Update Service and remove download file

--- main.go
+++ main.go
@@ -28,16 +28,17 @@ type server struct {
 }
 
 var (
-	address     = flag.String("address", "0.0.0.0", "Bind IP Address")
-	port        = flag.String("port", "8080", "Listen Port")
-	waDebug     = flag.String("wadebug", "", "Enable whatsmeow debug (INFO or DEBUG)")
-	logType     = flag.String("logtype", "console", "Type of log output (console or json)")
-	colorOutput = flag.Bool("color", false, "Enable colored output for console logs")
-	sslcert     = flag.String("sslcertificate", "", "SSL Certificate File")
-	sslprivkey  = flag.String("sslprivatekey", "", "SSL Certificate Private Key File")
-	adminToken  = flag.String("admintoken", "", "Security Token to authorize admin actions (list/create/remove users)")
-	showSwagger = flag.Bool("swagger", false, "Enable Swagger UI")
-	container   *sqlstore.Container
+	address      = flag.String("address", "0.0.0.0", "Bind IP Address")
+	port         = flag.String("port", "8080", "Listen Port")
+	waDebug      = flag.String("wadebug", "", "Enable whatsmeow debug (INFO or DEBUG)")
+	logType      = flag.String("logtype", "console", "Type of log output (console or json)")
+	colorOutput  = flag.Bool("color", false, "Enable colored output for console logs")
+	sslcert      = flag.String("sslcertificate", "", "SSL Certificate File")
+	sslprivkey   = flag.String("sslprivatekey", "", "SSL Certificate Private Key File")
+	adminToken   = flag.String("admintoken", "", "Security Token to authorize admin actions (list/create/remove users)")
+	showSwagger  = flag.Bool("swagger", false, "Enable Swagger UI")
+	downloadFile = flag.Bool("downloadfile", false, "Download file from Event")
+	container    *sqlstore.Container
 
 	killchannel   = make(map[int](chan bool))
 	userinfocache = cache.New(5*time.Minute, 10*time.Minute)
